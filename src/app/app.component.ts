7afa1cfa67da91ed22ab070d8f16c8ddf86dbd3b
Glenn Pinto <gvpinto@gmail.com>
2021-11-05 23:25:27 -0400

    Adding custom events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,20 @@ export class AppComponent {
   serverElements = [
     { type: "server", name: "TestServer", content: "Just a Test!" },
   ];
+
+  addServer(server: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: "server",
+      name: server.serverName,
+      content: server.serverContent,
+    });
+  }
+
+  addBlueprint(blueprint: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: "blueprint",
+      name: blueprint.serverName,
+      content: blueprint.serverContent,
+    });
+  }
 }
