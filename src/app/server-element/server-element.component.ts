d70ac7bc767c61570d448c519aa08c2d5969087e
Glenn Pinto <gvpinto@gmail.com>
2021-11-06 22:23:43 -0400

    79. Seeing Lifecycle Hooks in Action

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,17 @@
-import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewEncapsulation,
+} from "@angular/core";
 
 @Component({
   selector: "app-server-element",
@@ -6,9 +19,54 @@ import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
   styleUrls: ["./server-element.component.css"],
   encapsulation: ViewEncapsulation.Emulated, //default is Emulated, other options are native, none
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
   @Input("srvElement") element: { type: string; name: string; content: string };
-  constructor() {}
+  @Input() name: string;
+  constructor() {
+    console.log("constructor called");
+  }
 
-  ngOnInit(): void {}
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ngOnChanges called");
+    console.log(changes);
+  }
+  ngOnInit(): void {
+    console.log("ngOnInit called");
+  }
+
+  ngDoCheck(): void {
+    //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+    //Add 'implements DoCheck' to the class.
+    console.log("ngDoCheck called");
+  }
+
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit called");
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked called");
+  }
+
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit called");
+  }
+
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked called");
+  }
+
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy called");
+  }
 }
