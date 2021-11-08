fc1c18ae7ed8ce06a1e03b307fa62ec50727b990
Glenn Pinto <gvpinto@gmail.com>
2021-11-07 20:59:49 -0500

    Assignment 4: Practicing Property & Event Binding and View Encapsulation

---
+++ src/app/game-control/game-control.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+
+@Component({
+  selector: "app-game-control",
+  templateUrl: "./game-control.component.html",
+  styleUrls: ["./game-control.component.css"],
+})
+export class GameControlComponent implements OnInit {
+  @Output() streamTicks = new EventEmitter<{ value: number }>();
+  @Output() tickerListClear = new EventEmitter();
+  ticker: number = 0;
+  tickerRef = null;
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onStart(event: any): void {
+    this.tickerRef = setInterval(() => {
+      this.streamTicks.emit({ value: this.ticker++ });
+    }, 1000);
+  }
+
+  onStop(event: any): void {
+    clearInterval(this.tickerRef);
+    this.tickerListClear.emit();
+    this.ticker = 0;
+  }
+}
